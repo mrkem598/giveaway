b4d0beaf61f73a78e45675ba94ce168f4c101cdb
Moh Kemal <mohakemal9@gmail.com>
2017-07-15 15:17:47 -0400

    added view count react package

---
+++ app/components/ui/ViewCounter.js
@@ -0,0 +1,9 @@
+import MainMenu from './MainMenu'
+
+const ViewCounter = () =>
+    <div className="viewCounter">
+        <MainMenu className="home-page-menu"/>
+        <div id="homebox"></div>
+    </div>
+
+export default ViewCounter
